be7498e38c533047e57beb1e788bdb704fdb52ee
dodik123 <dodik.rismawan@greenholetech.com>
2020-06-10 12:50:47 +0700

    new_update

---
+++ template/lib/components/button/circularbutton.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class CicrularButton extends StatelessWidget {
+  
+  const CicrularButton({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text("Circular"),
+    );
+  }
+}
