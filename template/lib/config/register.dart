a435b89371156222781507596e4a297aa662a7db
dodik123 <dodik.rismawan@greenholetech.com>
2020-06-11 12:56:13 +0700

    new_update

--- template/lib/config/register.dart
+++ template/lib/config/register.dart
@@ -4,6 +4,8 @@ import 'package:provider/single_child_widget.dart';
 import 'package:template/modules/dashboard/controllers/dashboard.controller.dart';
 import 'package:template/modules/provider/controllers/provider.controller.dart';
 
+
+//untuk registrasi controller , jika controller tidak diregistrasi di sini maka tidak akan jalan
 class Register{
   static List<SingleChildWidget> providers = [
     ChangeNotifierProvider(create: (context)=> ProviderController()),
